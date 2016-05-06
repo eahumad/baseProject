8fd2e700e7e2d87a269b0376ba89e5cc6fdd32f1
milogrunge <emilio.ahumada.s@gmail.com>
2016-05-06 16:46:20 -0300

    - manejo de viestas mediante sessión implmentado  - diseño login base  - primeras librerías css y fuentes

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @author Emilio Ahumada
+ * @year 2015
+ */
+session_start();
+$usuario = isset( $_SESSION['usuario'] )?$_SESSION['usuario']:null;
+$page=null;
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>BaseProject</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <!-- Mobile support -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap core CSS -->
+  <link href="assets/css/bootstrap.css" rel="stylesheet">
+
+
+  <link href="assets/css/bootstrap-theme.css" rel="stylesheet">
+
+  <link href="assets/fonts/raleway/stylesheet.css" rel="stylesheet">
+  <link href="assets/css/login.css" rel="stylesheet">
+  <link href="assets/css/main.css" rel="stylesheet">
+</head>
+<body>
+<?php
+if( $usuario==null ){
+  require_once(dirname(__FILE__).'/views/login.php');
+} else {
+  //cargar vista menú base :3
+?>
+  <aside id="menu-wrapper">
+    <?php include("web_sections/menus.php"); ?>
+  </aside>
+  <section id="content-wrapper">
+<?php 
+  if($page!=null) {
+    try {
+      require('views/'.$page->archivo.'.php'); 
+    } catch (Exception $e) {
+      echo '<h1>No se pudo cargar la vista</h1>';
+    }
+  } else {
+    //CARGAR UN VISTA DE BIENVENIDA, IMPRIMIR ALGÚN MENSAJE O SIMPLEMENTE DEJAR VACÍO
+    echo '<h1>Bienvenido</h1>';
+  }
+?>
+  </section>
+<?php
+} //fin else
+?>
+</body>
+</html>
---
+++ views/login.php
@@ -0,0 +1,29 @@
+<div class="container login">
+  <div class="row">
+    <div class="col-lg-4 col-lg-offset4 col-md-4 col-md-offset-4 col-sm-12 col-xs-12">
+      <h1><span class="text-extra-light">Base<span><span class="text-light">Project</span></h1>
+      <h3>Arquitectura base para tu proyecto</h3>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-lg-4 col-lg-offset4 col-md-4 col-md-offset-4">
+      <form id="formLogin" role="form">
+        <div class="row">
+          <div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
+            <input type="text" id="username" name="username" placeholder="Usuario">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1">
+            <input type="password" id="password" name="password" placeholder="Contraseña">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <button type="button">Entrar</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
